ff3d4cef3a24674448fea234ca854ced7b76e671
supernative <pritomsarker314@gmail.com>
2024-07-27 10:53:28 -0400

    Create __init__.py

---
+++ object/__init__.py
@@ -0,0 +1,4 @@
+# This is the main branch of all possible object systems
+# The initial process includes 192k independently evolving columns
+# Each process is unitary relative to at least one other process
+# The object code defines the row dimensions.
