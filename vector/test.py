b9d749ce26edcb55746bfb3444d3bb10f6b349b5
supernative <pritomsarker314@gmail.com>
2024-07-27 16:13:43 -0400

    Update test.py

--- vector/test.py
+++ vector/test.py
@@ -1,1 +1,19 @@
 import time
+import os
+
+spectrum = 'spectrum.luminol'
+
+def print(string):
+    with open(spectrum, 'a') as f:
+        f.write("\n")
+        f.write(string)
+
+def while_true(condition):
+    query = input("~ ")
+    if query.strip() != condition:
+        print("{} ~".format(query))
+        while_true(condition)
+
+print("<{}>".format(time.time()))
+while_true("break")
+print("<{}>".format(time.time()))
