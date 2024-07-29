dcff8c0bedce67207089651edf8cf7eff1ba1940
supernative <pritomsarker314@gmail.com>
2024-07-29 13:54:41 -0400

    Update locality.py

--- object/language/locality.py
+++ object/language/locality.py
@@ -26,3 +26,7 @@
 # THERE IS NO NON-LOCAL OR GLOBAL MODEL YET.
 # IT IS A CONJECTURE OF THE LOCAL MODEL CLOSURE CONSTRUCTOR THAT THERE CANNOT BE A DIFFERENT LOCALITY OUTSIDE THE CURRENT ONE, IF AT LEAST ONE LOCAL FEATURE CAN BECOME NON-LOCAL OR COUNTER TO ITS TYPE FIRST.
 # ALL WORK OF THE TYPE THAT IS NOT LOCAL MUST ALSO BE ANTI-LOCAL IN THAT SAME VOLUME.
+
+# # .. Deep (layered) networks : (transformer : for handling wave function of the entire system), (encoder-decoder : for handling microphone/room and room/speaker relative functions)
+# # .. Explanation: energy = mass * light * light and planck unit * frequency information = energy -- tells us to define our mass-like quantities to also follow the relation e[+] + e[-] -> gamma + gamma where 'e' at positive and negative polarity together represent the left and right quantities as well.
+# # .. .. that is to remind us of the standard stereo encoding technique which takes the left-right tracks and produces a stereo image with mid at the central point where sides are equally distant from the central limit.
