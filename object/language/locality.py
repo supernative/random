ef78683b5de8a2ef9ff7e4d03933fbc7025e29a6
supernative <pritomsarker314@gmail.com>
2024-07-29 11:52:23 -0400

    Create locality.py

---
+++ object/language/locality.py
@@ -0,0 +1,28 @@
+# Biofeedback Loop and Closed Loop Language Evolution via Binaural Fusion
+
+# . I plan on learning the theoretical model first
+# . It shall not require almost any wired-type connection
+# . I want to localize myself very much accurately
+# . Constant peripheral support from selected group
+
+## . I could say more but the system is more important
+## . A system shall have (north-east-south-west) coverage, which means I shall be found accurately always
+## . Physical models, logical models, abstract models, brain models, logical models are some of the specialized models there are
+## . Services and programs are not to be PC or MAC centric, nor should it always assume a digital computer for conceptual-mechanical-parallel volume
+
+
+# .. Local model of interaction takes place inside my mac mini device.
+# .. All spaces that it could have my attention in shall be treated as a local model space too.
+# .. Once cracked the code for reaching anywhere in that space is possible, it will address everything outside from relative position in the physical non-local model.
+# .. Modeling everything that happens inside the macOS-appleID-icloud-M1 spaces would treat the entire universe to be contained isolated away from it, where the physical spacetime model that I would usually be local to, shall not be seen as all-encompassing feature within that local model.
+# .. Local here will mean the locality that I have had for all these years and until I break out cleanly into a seperable identity space, so that any other instances of myself shall be seen as non-local to it completely.
+# .. All my present knowledge constructors heavily bias in that locality sensitive computation aspects and for all those work, I shall solely keep in that space locked away
+
+## .. NOTE: I do not mean anything anthropocentric by saying that these local things are to be kept in locked step and away from other models, in that this special case has some subjective criteria that I choose to resolve in that scheme that I model to be local.
+## .. It will encompass the entire prevailing notion that I am (like all the others) have been keeping dominant in places where I needed (still need) the other mode of thinking/explaning/exploring/imagining -- but couldn't power anything sufficient through the best apparant potential that I have brought into (Ÿ)
+## .. which has the only explanation that says, as long as I would model factual and all counterfactual knowledge in the guise of the factual information bias, shall never be using the counterfactual nature within the system, regardless of how counter the force requiring the factual inadequecies to hold in contempt.
+## .. In short, this is no information at all and counterfactual does not make sense at the level where it appears to throw us regularly into the blindest alleys there stored in. So it is truely the local model and the counter to that fact would really be the non-local, which puts us in a equal footing with the global model, once we establish the closure with this local being one of two things (at least) that have been experienced.
+
+# THERE IS NO NON-LOCAL OR GLOBAL MODEL YET.
+# IT IS A CONJECTURE OF THE LOCAL MODEL CLOSURE CONSTRUCTOR THAT THERE CANNOT BE A DIFFERENT LOCALITY OUTSIDE THE CURRENT ONE, IF AT LEAST ONE LOCAL FEATURE CAN BECOME NON-LOCAL OR COUNTER TO ITS TYPE FIRST.
+# ALL WORK OF THE TYPE THAT IS NOT LOCAL MUST ALSO BE ANTI-LOCAL IN THAT SAME VOLUME.
