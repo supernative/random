e6cf89ae9fc1bbe8cce238883e110dbf82ebcad5
supernative <pritomsarker314@gmail.com>
2024-07-30 23:19:39 -0400

    Create interface.py

---
+++ object/mixer/interface.py
@@ -0,0 +1,4 @@
+# Manages USB <-> BUFFER over serial communication protocol.
+# All sampler/encoder/constructor related functions are managed here.
+# A constructor will create copies of the frames and make a callable accessible for iterating over the voltages.
+# Samples are bytes.
