57e45b939be1be45d8b4692f91f7b88f8c73498c
supernative <pritomsarker314@gmail.com>
2024-07-31 22:24:58 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+"""
 from signal import check, tell, encode, decode
 
 class Program:
@@ -21,3 +22,42 @@ class Program:
 
     def store(self):
         tell(self.name, self.track, self.length)
+"""
+# NEW VERSION
+
+"""
+model description:
+
+- a < x < b # open intervals in limit (0, 1)
+- measure function : left elements , right elements >> rational choice, irrational choice
+this happens by elected bodies (nodel?)
+
+f(the programming, storage space, access points)
+- holds code until storage is found
+- creates access point with copy kept
+- image is constructed
+
+Note: the function (f) is said to be the HOST machine, which allows the programming.
+
+
+(algorithm)
+
+. start scanning local geometry
+. every change encodes (l + r) -> (m, s)
+.. m maps things that are centered around the origin
+.. s maps things that are edged around the origin.
+. compute the mass change at the current graph
+. if change is negative, keep scanning more
+. else, if greater, translate to the next point
+.. else, if same, remain still.
+
+graphs could be structured like so:
+
+. every node is a mid point
+. every edge is a side chain
+
+a link is a map between two tracks in a stereo image.
+. stereo image : left, right -> mid, side
+
+node points are accessible via the linker
+"""
